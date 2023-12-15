ef4f262fc738c2e616ed4e4be8e7e5174266340b
Har1bo <Amal@mail.ru>
2023-12-15 13:42:51 +0300

    Первый коммит

--- src/App.js
+++ src/App.js
@@ -3,31 +3,63 @@ import './index.scss';
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: 'Настоящее имя Macan?',
+    variants: ['Андрей', 'Амаль', 'Павел'],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: 'Когда родился Macan?',
+    variants: ['3.02.2001', '06.01.2002', '27.01.2000'],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
+    title: 'Кем вдохновлялся Macan?',
+    variants: ['Баста', 'Тимати', 'Гуф'],
+    correct: 0,
+  },
+  {
+    title: 'Предыдущий псевдоним Macan?',
+    variants: ['Haribo', 'Mavashi', ' Young Chaser'],
     correct: 2,
   },
+  {
+    title: 'С какого трека строки : Бьёмся о скалы ночами, Неудачи нас тащат на дно. Тут тощие пацы с окраин, Что пересмотрели кино.',
+    variants: ['Харибо', 'Кино', 'Бенз'],
+    correct: 2,
+  },
+  {
+    title: 'Сколько совместных треков с SCIRENA',
+    variants: ['2', '1', '3'],
+    correct: 1,
+  },
+  {
+    title: 'С кем записан трек "Не умеешь,не люби"',
+    variants: ['HammAli & Navai', 'Ramil', 'Xcho'],
+    correct: 0,
+  },
+  {
+    title: 'Из какого трека строчка : Все мы тут свои, пока от нас чего-то ждут',
+    variants: ['Гири', 'IVL', 'Hollywood'],
+    correct: 2,
+  },
+  {
+    title: 'Какой из треков не принадлежит Macan?',
+    variants: ['Shazam', 'Вороны', '77'],
+    correct: 1,
+  },
+  {
+    title: 'Тебе нраявятся треки Macan?',
+    variants: ['Нет', 'Да', 'Нет, у меня плохой вкус'],
+    correct: 1,
+  },
+
 ];
 
 function Result({correct}) {
   return (
     <div className="result">
       <img class="haribo" src="https://avatars.mds.yandex.net/i?id=3ec845d9af76119b6252d0aabd959d253deadf53-9699789-images-thumbs&n=13" />
-      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <h2>Вы отгадали {correct} ответов из {questions.length}</h2>
       <a href="/"><button>Попробовать снова</button></a>
     </div>
   );
